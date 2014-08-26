cf32877cf7f841c6bb83450575850c537769c8a1
David Andersson <david@spamdrain.net>
2014-08-26 15:06:52 +0200

    Sample which tests Quartz and coregraphics

---
+++ CustomAnimatableProperty/src/org/robovm/samples/customanimateproperty/CustomAnimatableProperty.java
@@ -0,0 +1,69 @@
+ /*
+ * Copyright (C) 2014 Trillian Mobile AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * 
+ * Portions of this code is based on Apple Inc's QuickContacts sample (v1.0)
+ * which is copyright (C) 2008-2013 Apple Inc.
+ * 
+ * The view controller creates a few bulb views which host the custom layer subclass.
+ */
+
+package org.robovm.samples.customanimateproperty;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.foundation.NSDictionary;
+import org.robovm.apple.foundation.NSString;
+import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
+import org.robovm.apple.uikit.UIColor;
+import org.robovm.apple.uikit.UINavigationController;
+import org.robovm.apple.uikit.UIScreen;
+import org.robovm.apple.uikit.UIWindow;
+import org.robovm.samples.customanimateproperty.viewcontrollers.ViewController;
+
+/**
+ * Main class
+ */
+public class CustomAnimatableProperty extends UIApplicationDelegateAdapter {
+
+    private UIWindow window = null;
+
+    @Override
+    public boolean didFinishLaunching(UIApplication application,
+            NSDictionary<NSString, ?> launchOptions) {
+
+        window = new UIWindow(UIScreen.getMainScreen().getBounds());
+        window.setBackgroundColor(UIColor.colorLightGray());
+        UINavigationController navigationController = new UINavigationController(
+                application.addStrongRef(new ViewController()));
+        window.setRootViewController(navigationController);
+        window.makeKeyAndVisible();
+
+        // Ties UIWindow instance together with UIApplication object on the
+        // Objective C side of things
+        // Basically meaning that it wont be GC:ed on the java side until it is
+        // on the Objective C side
+        application.addStrongRef(window);
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(args, null, CustomAnimatableProperty.class);
+        pool.close();
+    }
+    
+
+}
---
+++ CustomAnimatableProperty/src/org/robovm/samples/customanimateproperty/viewcontrollers/ViewController.java
@@ -0,0 +1,88 @@
+ /*
+ * Copyright (C) 2014 Trillian Mobile AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * 
+ * Portions of this code is based on Apple Inc's QuickContacts sample (v1.0)
+ * which is copyright (C) 2008-2013 Apple Inc.
+ * 
+ * The view controller creates a few bulb views which host the custom layer subclass.
+ */
+
+package org.robovm.samples.customanimateproperty.viewcontrollers;
+
+import org.robovm.apple.coregraphics.CGPoint;
+import org.robovm.apple.coregraphics.CGRect;
+import org.robovm.apple.uikit.UIAlertView;
+import org.robovm.apple.uikit.UIColor;
+import org.robovm.apple.uikit.UIImage;
+import org.robovm.apple.uikit.UIScreen;
+import org.robovm.apple.uikit.UIViewController;
+import org.robovm.samples.customanimateproperty.BulbView;
+
+/**
+ * The view controller creates a few blub views which host the custom layer subclass.
+ */
+public class ViewController extends UIViewController{
+	
+	public void viewDidLoad() {
+	    super.viewDidLoad();
+	    this.getView().setFrame(new CGRect(0.0, 0.0, 320, 568));
+		// Do any additional setup after loading the view, typically from a nib.
+	    
+	    this.getView().setBackgroundColor(UIColor.colorYellow());
+	    
+	    // Load the bulb image.
+	    UIImage bulb = UIImage.createFromBundle("bulb.png");
+	    
+	    // Base the size of the bulb views on the screen size.
+	    double screenWidth = UIScreen.getMainScreen().getBounds().size().width();
+	    double screenHeight = UIScreen.getMainScreen().getBounds().size().height();
+	    double bulbHeight = screenHeight / 2.1;
+	    
+	    // Maintain image proportions by basing width on the width-to-height ratio.
+	    double widthHeightRatio = bulb.getSize().width() / bulb.getSize().height();
+	    System.err.printf("widthHeightRatio: %f", widthHeightRatio );
+	    double bulbWidth = ( bulbHeight * widthHeightRatio ) * 1.5; // times 1.5 to fatten up the bulb for added visual effect.
+
+	    // Define our view hierarchy.
+	    BulbView bulbview;
+	    CGRect startingFrame = new CGRect( 0, 0, bulbWidth, bulbHeight );
+	    bulbview = new BulbView(startingFrame);
+	    bulbview.setColor(UIColor.colorRed());
+	    bulbview.setCenter(new CGPoint( .25 * screenWidth, .20 * screenHeight ));
+	    this.getView().addSubview(bulbview);
+	    bulbview = new BulbView(startingFrame);
+	    bulbview.setColor(UIColor.colorGreen());
+	    bulbview.setCenter(new CGPoint( .5 * screenWidth, .5 * screenHeight ));
+	    this.getView().addSubview(bulbview);
+	    bulbview = new BulbView(startingFrame);
+	    bulbview.setColor(UIColor.colorBlue());
+	    bulbview.setCenter(new CGPoint( .75 * screenWidth, .80 * screenHeight ));
+	    this.getView().addSubview(bulbview);
+
+	    // Display an alert view that explains usability.
+	    UIAlertView alert = new UIAlertView("Tap bulbs to animate.", null, null, "OK", new String[0]);
+	    // optional - add more buttons:
+	    //[alert addButtonWithTitle:@"Ok"];
+	    alert.show();
+	}
+	
+	@Override
+	public void didReceiveMemoryWarning()
+	{
+	    didReceiveMemoryWarning();
+	    // Dispose of any resources that can be recreated.
+	}
+	
+}
