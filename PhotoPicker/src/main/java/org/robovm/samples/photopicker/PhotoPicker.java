779f3a1704f2b4ac594927ba93143aeac7b8c385
BlueRiverInteractive <blueriverteam@gmail.com>
2015-04-18 17:40:57 +0200

    Port apple sample 'PhotoPicker' (#37).

---
+++ PhotoPicker/src/main/java/org/robovm/samples/photopicker/PhotoPicker.java
@@ -0,0 +1,20 @@
+package org.robovm.samples.photopicker;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
+import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+
+public class PhotoPicker extends UIApplicationDelegateAdapter {
+
+    @Override
+    public boolean didFinishLaunching(UIApplication application, UIApplicationLaunchOptions launchOptions) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        try (NSAutoreleasePool pool = new NSAutoreleasePool()) {
+            UIApplication.main(args, null, PhotoPicker.class);
+        }
+    }
+}
