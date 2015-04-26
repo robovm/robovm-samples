647ebaf80963bfafc9c31a05c3fc0fdfebcffbc6
BlueRiverInteractive <blueriverteam@gmail.com>
2015-04-26 02:02:19 +0200

    Updated MessageComposer sample (#41).

--- MessageComposer/src/main/java/org/robovm/samples/messagecomposer/MessageComposer.java
+++ MessageComposer/src/main/java/org/robovm/samples/messagecomposer/MessageComposer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 RoboVM AB
+ * Copyright (C) 2013-2015 RoboVM AB
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,42 +16,23 @@
  * Portions of this code is based on Apple Inc's MessageComposer sample (v1.2)
  * which is copyright (C) 2010-2013 Apple Inc.
  */
-
 package org.robovm.samples.messagecomposer;
 
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
-import org.robovm.apple.uikit.UIScreen;
-import org.robovm.apple.uikit.UIWindow;
-import org.robovm.samples.messagecomposer.viewcontrollers.MessageComposerViewController;
+import org.robovm.apple.uikit.UIApplicationLaunchOptions;
 
 public class MessageComposer extends UIApplicationDelegateAdapter {
-    private UIWindow window;
-    private MessageComposerViewController viewController;
 
     @Override
-    public void didFinishLaunching (UIApplication application) {
-        // Set up the view controller.
-        viewController = new MessageComposerViewController();
-
-        // Create a new window at screen size.
-        window = new UIWindow(UIScreen.getMainScreen().getBounds());
-        // Set our viewcontroller as the root controller for the window.
-        window.setRootViewController(viewController);
-        // Make the window visible.
-        window.makeKeyAndVisible();
-
-        /*
-         * Retains the window object until the application is deallocated. Prevents Java GC from collecting the window object too
-         * early.
-         */
-        addStrongRef(window);
+    public boolean didFinishLaunching(UIApplication application, UIApplicationLaunchOptions launchOptions) {
+        return true;
     }
 
-    public static void main (String[] args) {
-        NSAutoreleasePool pool = new NSAutoreleasePool();
-        UIApplication.main(args, null, MessageComposer.class);
-        pool.close();
+    public static void main(String[] args) {
+        try (NSAutoreleasePool pool = new NSAutoreleasePool()) {
+            UIApplication.main(args, null, MessageComposer.class);
+        }
     }
 }
