0949c7efb35dadf1c9f74aa11aef18531ae435ad
BlueRiverInteractive <blueriverteam@gmail.com>
2015-05-07 14:09:12 +0200

    Update LazyTableImages with storyboard (#41).

--- LazyTableImages/src/main/java/org/robovm/samples/lazytableimages/LazyTableImages.java
+++ LazyTableImages/src/main/java/org/robovm/samples/lazytableimages/LazyTableImages.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 RoboVM AB
+ * Copyright (C) 2013-2015 RoboVM AB
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
  * Portions of this code is based on Apple Inc's LazyTableImages sample (v1.5)
  * which is copyright (C) 2010-2014 Apple Inc.
  */
-
 package org.robovm.samples.lazytableimages;
 
 import java.lang.ref.WeakReference;
@@ -37,19 +36,13 @@ import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
 import org.robovm.apple.uikit.UIApplicationLaunchOptions;
 import org.robovm.apple.uikit.UINavigationController;
-import org.robovm.apple.uikit.UIScreen;
-import org.robovm.apple.uikit.UIWindow;
 import org.robovm.objc.block.VoidBlock1;
 import org.robovm.samples.lazytableimages.operation.ParseOperation;
-import org.robovm.samples.lazytableimages.viewcontrollers.RootViewController;
+import org.robovm.samples.lazytableimages.ui.RootViewController;
 
 public class LazyTableImages extends UIApplicationDelegateAdapter {
     // the http URL used for fetching the top iOS paid apps on the App Store
     private static final String TOP_PAID_APPS_FEED = "http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=75/xml";
-
-    private UIWindow window;
-    private UINavigationController navController;
-    private RootViewController rootViewController;
 
     // the queue to run our "ParseOperation"
     private NSOperationQueue queue;
@@ -58,46 +51,41 @@ public class LazyTableImages extends UIApplicationDelegateAdapter {
     private NSMutableData appListData;
 
     @Override
-    public boolean didFinishLaunching (UIApplication application, UIApplicationLaunchOptions launchOptions) {
-        // Set up the view controller.
-        rootViewController = new RootViewController();
-        navController = new UINavigationController(rootViewController);
-
-        // Create a new window at screen size.
-        window = new UIWindow(UIScreen.getMainScreen().getBounds());
-        // Set our viewcontroller as the root controller for the window.
-        window.setRootViewController(navController);
-        // Make the window visible.
-        window.makeKeyAndVisible();
-
-        /*
-         * Retains the window object until the application is deallocated. Prevents Java GC from collecting the window object too
-         * early.
-         */
-        addStrongRef(window);
-
+    public boolean didFinishLaunching(UIApplication application, UIApplicationLaunchOptions launchOptions) {
         NSURLRequest urlRequest = new NSURLRequest(new NSURL(TOP_PAID_APPS_FEED));
         appListFeedConnection = new NSURLConnection(urlRequest, new NSURLConnectionDataDelegateAdapter() {
-            // The following are delegate methods for NSURLConnection. Similar to callback functions, this is how
-            // the connection object, which is working in the background, can asynchronously communicate back to
-            // its delegate on the thread from which it was started - in this case, the main thread.
+            // The following are delegate methods for NSURLConnection. Similar
+            // to callback functions, this is how
+            // the connection object, which is working in the background, can
+            // asynchronously communicate back to
+            // its delegate on the thread from which it was started - in this
+            // case, the main thread.
 
-            /** Called when enough data has been read to construct an NSURLResponse object. */
+            /**
+             * Called when enough data has been read to construct an
+             * NSURLResponse object.
+             */
             @Override
-            public void didReceiveResponse (NSURLConnection connection, NSURLResponse response) {
+            public void didReceiveResponse(NSURLConnection connection, NSURLResponse response) {
                 appListData = new NSMutableData();// start off with new data
             }
 
-            /** Called with a single immutable NSData object to the delegate, representing the next portion of the data loaded from
-             * the connection. */
+            /**
+             * Called with a single immutable NSData object to the delegate,
+             * representing the next portion of the data loaded from the
+             * connection.
+             */
             @Override
-            public void didReceiveData (NSURLConnection connection, NSData data) {
+            public void didReceiveData(NSURLConnection connection, NSData data) {
                 appListData.append(data); // append incoming data
             }
 
-            /** Will be called at most once, if an error occurs during a resource load. No other callbacks will be made after. */
+            /**
+             * Will be called at most once, if an error occurs during a resource
+             * load. No other callbacks will be made after.
+             */
             @Override
-            public void didFail (NSURLConnection connection, NSError error) {
+            public void didFail(NSURLConnection connection, NSError error) {
                 UIApplication.getSharedApplication().setNetworkActivityIndicatorVisible(false);
 
                 handleError(error);
@@ -105,10 +93,12 @@ public class LazyTableImages extends UIApplicationDelegateAdapter {
                 appListFeedConnection = null; // release our connection
             }
 
-            /** Called when all connection processing has completed successfully, before the delegate is released by the
-             * connection. */
+            /**
+             * Called when all connection processing has completed successfully,
+             * before the delegate is released by the connection.
+             */
             @Override
-            public void didFinishLoading (NSURLConnection connection) {
+            public void didFinishLoading(NSURLConnection connection) {
                 appListFeedConnection = null; // release our connection
 
                 UIApplication.getSharedApplication().setNetworkActivityIndicatorVisible(false);
@@ -116,36 +106,47 @@ public class LazyTableImages extends UIApplicationDelegateAdapter {
                 // create the queue to run our ParseOperation
                 queue = new NSOperationQueue();
 
-                // create an ParseOperation (NSOperation subclass) to parse the RSS feed data
+                // create an ParseOperation (NSOperation subclass) to parse the
+                // RSS feed data
                 // so that the UI is not blocked
                 ParseOperation parser = new ParseOperation(appListData);
                 parser.setErrorHandler(new VoidBlock1<NSError>() {
                     @Override
-                    public void invoke (final NSError parseError) {
+                    public void invoke(final NSError parseError) {
                         DispatchQueue.getMainQueue().async(new Runnable() {
                             @Override
-                            public void run () {
+                            public void run() {
                                 handleError(parseError);
                             }
                         });
                     }
                 });
 
-                // Referencing parser from within its completionBlock would create a retain cycle.
+                // Referencing parser from within its completionBlock would
+                // create a retain cycle.
                 final WeakReference<ParseOperation> weakParser = new WeakReference<ParseOperation>(parser);
                 weakParser.get().setCompletionBlock(new Runnable() {
                     @Override
-                    public void run () {
+                    public void run() {
                         if (weakParser.get().getAppRecordList() != null) {
-                            // The completion block may execute on any thread. Because operations
-                            // involving the UI are about to be performed, make sure they execute
+                            // The completion block may execute on any thread.
+                            // Because operations
+                            // involving the UI are about to be performed, make
+                            // sure they execute
                             // on the main thread.
                             DispatchQueue.getMainQueue().async(new Runnable() {
                                 @Override
-                                public void run () {
+                                public void run() {
+                                    // The root rootViewController is the only
+                                    // child of the navigation
+                                    // controller, which is the window's
+                                    // rootViewController.
+                                    RootViewController rootViewController = (RootViewController) ((UINavigationController) getWindow()
+                                            .getRootViewController()).getTopViewController();
                                     rootViewController.setEntries(weakParser.get().getAppRecordList());
 
-                                    // tell our table view to reload its data, now that parsing has completed
+                                    // tell our table view to reload its data,
+                                    // now that parsing has completed
                                     rootViewController.getTableView().reloadData();
                                 }
                             });
@@ -156,18 +157,24 @@ public class LazyTableImages extends UIApplicationDelegateAdapter {
                     }
                 });
 
-                queue.addOperation(parser); // this will start the "ParseOperation"
+                queue.addOperation(parser); // this will start the
+                                            // "ParseOperation"
 
-                // ownership of appListData has been transferred to the parse operation
+                // ownership of appListData has been transferred to the parse
+                // operation
                 // and should no longer be referenced in this thread
                 appListData = null;
             }
         });
 
-        // Test the validity of the connection object. The most likely reason for the connection object
-        // to be nulll is a malformed URL, which is a programmatic error easily detected during development
-        // If the URL is more dynamic, then you should implement a more flexible validation technique, and
-        // be able to both recover from errors and communicate problems to the user in an unobtrusive manner.
+        // Test the validity of the connection object. The most likely reason
+        // for the connection object
+        // to be nulll is a malformed URL, which is a programmatic error easily
+        // detected during development
+        // If the URL is more dynamic, then you should implement a more flexible
+        // validation technique, and
+        // be able to both recover from errors and communicate problems to the
+        // user in an unobtrusive manner.
         if (appListFeedConnection == null) {
             throw new RuntimeException("Failure to create URL connection.");
         }
@@ -178,17 +185,21 @@ public class LazyTableImages extends UIApplicationDelegateAdapter {
         return true;
     }
 
-    /** Reports any error with an alert which was received from connection or loading failures.
-     * @param error */
-    private void handleError (NSError error) {
+    /**
+     * Reports any error with an alert which was received from connection or
+     * loading failures.
+     * 
+     * @param error
+     */
+    private void handleError(NSError error) {
         String errorMessage = error.getLocalizedDescription();
         UIAlertView alertView = new UIAlertView("Cannot Show Top Paid Apps", errorMessage, null, "OK");
         alertView.show();
     }
 
-    public static void main (String[] args) {
-        NSAutoreleasePool pool = new NSAutoreleasePool();
-        UIApplication.main(args, null, LazyTableImages.class);
-        pool.close();
+    public static void main(String[] args) {
+        try (NSAutoreleasePool pool = new NSAutoreleasePool()) {
+            UIApplication.main(args, null, LazyTableImages.class);
+        }
     }
 }
--- LazyTableImages/src/main/java/org/robovm/samples/lazytableimages/ui/RootViewController.java
+++ LazyTableImages/src/main/java/org/robovm/samples/lazytableimages/ui/RootViewController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 RoboVM AB
+ * Copyright (C) 2013-2015 RoboVM AB
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
  * which is copyright (C) 2010-2014 Apple Inc.
  */
 
-package org.robovm.samples.lazytableimages.viewcontrollers;
+package org.robovm.samples.lazytableimages.ui;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,10 +33,12 @@ import org.robovm.apple.uikit.UITableView;
 import org.robovm.apple.uikit.UITableViewCell;
 import org.robovm.apple.uikit.UITableViewCellStyle;
 import org.robovm.apple.uikit.UITableViewController;
+import org.robovm.objc.annotation.CustomClass;
 import org.robovm.objc.block.VoidBlock1;
 import org.robovm.samples.lazytableimages.operation.AppRecord;
 import org.robovm.samples.lazytableimages.operation.IconDownloader;
 
+@CustomClass("RootViewController")
 public class RootViewController extends UITableViewController {
     private static final int CUSTOM_ROW_COUNT = 7;
     private static final String CELL_IDENTIFIER = "LazyTableCell";
@@ -44,39 +46,47 @@ public class RootViewController extends UITableViewController {
 
     public class MyTableViewCell extends UITableViewCell {
         @Override
-        protected long init (UITableViewCellStyle style, String reuseIdentifier) {
-            // ignore the style argument and force the creation with style UITableViewCellStyleSubtitle
+        protected long init(UITableViewCellStyle style, String reuseIdentifier) {
+            // TODO use constructor when fixed
+            // ignore the style argument and force the creation with style
+            // UITableViewCellStyleSubtitle
             return super.init(UITableViewCellStyle.Subtitle, reuseIdentifier);
         }
     }
 
     private List<AppRecord> entries = new ArrayList<>();
-    private final Map<Integer, IconDownloader> imageDownloadsInProgress = new HashMap<>();
+    private Map<Integer, IconDownloader> imageDownloadsInProgress;
 
-    public RootViewController () {
-        setTitle("Top Paid Apps");
+    @Override
+    public void viewDidLoad() {
+        super.viewDidLoad();
 
+        getTableView().registerReusableCellClass(MyTableViewCell.class, CELL_IDENTIFIER);
+        getTableView().registerReusableCellClass(MyTableViewCell.class, PLACEHOLDER_CELL_IDENTIFIER);
         getTableView().setDelegate(new UIScrollViewDelegateAdapter() {
             /** Load images for all onscreen rows when scrolling is finished. */
             @Override
-            public void didEndDragging (UIScrollView scrollView, boolean decelerate) {
+            public void didEndDragging(UIScrollView scrollView, boolean decelerate) {
                 if (!decelerate) {
                     loadImagesForOnscreenRows();
                 }
             }
 
-            /** When scrolling stops, proceed to load the app icons that are on screen. */
+            /**
+             * When scrolling stops, proceed to load the app icons that are on
+             * screen.
+             */
             @Override
-            public void didEndDecelerating (UIScrollView scrollView) {
+            public void didEndDecelerating(UIScrollView scrollView) {
                 loadImagesForOnscreenRows();
             }
         });
 
-        getTableView().registerReusableCellClass(MyTableViewCell.class, CELL_IDENTIFIER);
-        getTableView().registerReusableCellClass(MyTableViewCell.class, PLACEHOLDER_CELL_IDENTIFIER);
+        entries = new ArrayList<>();
+        imageDownloadsInProgress = new HashMap<>();
     }
 
-    private void terminateAllDownloads () {
+    private void terminateAllDownloads() {
         // terminate all pending download connections
         for (IconDownloader download : imageDownloadsInProgress.values()) {
             download.cancelDownload();
@@ -84,16 +94,19 @@ public class RootViewController extends UITableViewController {
         imageDownloadsInProgress.clear();
     }
 
-    /** If this view controller is going away, we need to cancel all outstanding downloads. */
+    /**
+     * If this view controller is going away, we need to cancel all outstanding
+     * downloads.
+     */
     @Override
-    protected void dispose (boolean finalizing) {
+    protected void dispose(boolean finalizing) {
         // terminate all pending download connections
         terminateAllDownloads();
         super.dispose(finalizing);
     }
 
     @Override
-    public void didReceiveMemoryWarning () {
+    public void didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning();
 
         // terminate all pending download connections
@@ -102,7 +115,7 @@ public class RootViewController extends UITableViewController {
 
     /** Customize the number of rows in the table view. */
     @Override
-    public long getNumberOfRowsInSection (UITableView tableView, long section) {
+    public long getNumberOfRowsInSection(UITableView tableView, long section) {
         int count = entries.size();
 
         // if there's no data yet, return enough rows to fill the screen
@@ -113,32 +126,34 @@ public class RootViewController extends UITableViewController {
     }
 
     @Override
-    public UITableViewCell getCellForRow (UITableView tableView, NSIndexPath indexPath) {
+    public UITableViewCell getCellForRow(UITableView tableView, NSIndexPath indexPath) {
         MyTableViewCell cell = null;
 
         int nodeCount = entries.size();
 
         if (nodeCount == 0 && indexPath.getRow() == 0) {
             // add a placeholder cell while waiting on table data
-            cell = (MyTableViewCell)getTableView().dequeueReusableCell(PLACEHOLDER_CELL_IDENTIFIER, indexPath);
+            cell = (MyTableViewCell) getTableView().dequeueReusableCell(PLACEHOLDER_CELL_IDENTIFIER, indexPath);
             cell.getDetailTextLabel().setText("Loading…");
         } else {
-            cell = (MyTableViewCell)getTableView().dequeueReusableCell(CELL_IDENTIFIER, indexPath);
+            cell = (MyTableViewCell) getTableView().dequeueReusableCell(CELL_IDENTIFIER, indexPath);
 
             // Leave cells empty if there's no data yet
             if (nodeCount > 0) {
                 // Set up the cell representing the app
-                AppRecord appRecord = entries.get((int)indexPath.getRow());
+                AppRecord appRecord = entries.get((int) indexPath.getRow());
 
                 cell.getTextLabel().setText(appRecord.appName);
                 cell.getDetailTextLabel().setText(appRecord.artist);
 
-                // Only load cached images; defer new downloads until scrolling ends
+                // Only load cached images; defer new downloads until scrolling
+                // ends
                 if (appRecord.appIcon == null) {
                     if (!getTableView().isDragging() && !getTableView().isDecelerating()) {
                         startIconDownload(appRecord, indexPath);
                     }
-                    // if a download is deferred or in progress, return a placeholder image
+                    // if a download is deferred or in progress, return a
+                    // placeholder image
                     cell.getImageView().setImage(UIImage.create("Placeholder.png"));
                 } else {
                     cell.getImageView().setImage(appRecord.appIcon);
@@ -148,32 +163,35 @@ public class RootViewController extends UITableViewController {
         return cell;
     }
 
-    private void startIconDownload (AppRecord appRecord, final NSIndexPath indexPath) {
-        IconDownloader iconDownloader = imageDownloadsInProgress.get((int)indexPath.getRow());
+    private void startIconDownload(AppRecord appRecord, final NSIndexPath indexPath) {
+        IconDownloader iconDownloader = imageDownloadsInProgress.get((int) indexPath.getRow());
         if (iconDownloader == null) {
             iconDownloader = new IconDownloader(appRecord, new VoidBlock1<AppRecord>() {
                 @Override
-                public void invoke (AppRecord a) {
+                public void invoke(AppRecord a) {
                     UITableViewCell cell = getTableView().getCellForRow(indexPath);
 
                     // Display the newly loaded image
                     cell.getImageView().setImage(a.appIcon);
                     // Remove the IconDownloader from the in progress list.
                     // This will result in it being deallocated.
-                    imageDownloadsInProgress.remove((int)indexPath.getRow());
+                    imageDownloadsInProgress.remove((int) indexPath.getRow());
                 }
             });
-            imageDownloadsInProgress.put((int)indexPath.getRow(), iconDownloader);
+            imageDownloadsInProgress.put((int) indexPath.getRow(), iconDownloader);
             iconDownloader.startDownload();
         }
     }
 
-    /** This method is used in case the user scrolled into a set of cells that don't have their app icons yet. */
-    private void loadImagesForOnscreenRows () {
+    /**
+     * This method is used in case the user scrolled into a set of cells that
+     * don't have their app icons yet.
+     */
+    private void loadImagesForOnscreenRows() {
         if (entries.size() > 0) {
             NSArray<NSIndexPath> visiblePaths = getTableView().getIndexPathsForVisibleRows();
             for (NSIndexPath indexPath : visiblePaths) {
-                AppRecord appRecord = entries.get((int)indexPath.getRow());
+                AppRecord appRecord = entries.get((int) indexPath.getRow());
 
                 // Avoid the app icon download if the app already has an icon
                 if (appRecord.appIcon == null) {
@@ -183,7 +201,7 @@ public class RootViewController extends UITableViewController {
         }
     }
 
-    public void setEntries (List<AppRecord> entries) {
+    public void setEntries(List<AppRecord> entries) {
         this.entries = entries;
     }
 }
