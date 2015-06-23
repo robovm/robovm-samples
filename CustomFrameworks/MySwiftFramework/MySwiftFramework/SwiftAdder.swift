df8103ffa05dd1e933782657c1c59ad3c9197f39
badlogic <badlogicgames@gmail.com>
2015-06-23 17:01:18 +0200

    Initial commit of custom framework sample, fixed Footprint sample

---
+++ CustomFrameworks/MySwiftFramework/MySwiftFramework/SwiftAdder.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftAdder.swift
+//  MySwiftFramework
+//
+//  Created by Mario Zechner on 23/06/15.
+//  Copyright (c) 2015 Mario Zechner. All rights reserved.
+//
+
+import Foundation
+
+@objc
+public class SwiftAdder {
+    public func add(a: Int32, b: Int32) -> Int32 {
+        return a + b;
+    }
+}
