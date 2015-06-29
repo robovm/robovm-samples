54523f80fd67727c8aafea972ccf990752e1c294
badlogic <badlogicgames@gmail.com>
2015-06-29 15:18:25 +0200

    Added resource loading. swift framework contains a storyboard, view controller fetches it from the framework bundle

--- CustomFrameworks/src/main/java/org/robovm/samples/customframeworks/MyViewController.java
+++ CustomFrameworks/src/main/java/org/robovm/samples/customframeworks/MyViewController.java
@@ -1,9 +1,10 @@
 package org.robovm.samples.customframeworks;
 
+import org.robovm.apple.foundation.NSBundle;
 import org.robovm.apple.uikit.UILabel;
+import org.robovm.apple.uikit.UIStoryboard;
 import org.robovm.apple.uikit.UITextField;
 import org.robovm.apple.uikit.UIViewController;
-import org.robovm.objc.ObjCRuntime;
 import org.robovm.objc.annotation.CustomClass;
 import org.robovm.objc.annotation.IBAction;
 import org.robovm.objc.annotation.IBOutlet;
@@ -12,9 +13,18 @@ import org.robovm.objc.annotation.IBOutlet;
 public class MyViewController extends UIViewController {
 	private UILabel result;
 	private UITextField val1;
-	private UITextField val2;	    
-    
-    @IBOutlet
+	private UITextField val2;
+
+    @Override
+	public void viewDidLoad() {
+		super.viewDidLoad();
+		// this just demonstrates how to load a resources
+		// from a framework. The storyboard is not really used.
+		NSBundle bundle = NSBundle.getBundle("org.robovm.MySwiftFramework");		
+		UIStoryboard storyboard = UIStoryboard.create("Storyboard", bundle);
+	}
+
+	@IBOutlet
     public void setResult(UILabel result) {
     	this.result = result;
     	this.result.setText("Enter 2 numbers");
