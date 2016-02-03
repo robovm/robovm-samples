29fd568780ee8205a6fd63c97ee6e6de3a6fc1b1
BlueRiverInteractive <blueriverteam@gmail.com>
2016-02-03 16:32:56 +0100

    Add license headers.

--- robopods/flurry/ios/src/main/java/org/robovm/samples/robopods/flurry/ios/ui/MainMenuViewController.java
+++ robopods/flurry/ios/src/main/java/org/robovm/samples/robopods/flurry/ios/ui/MainMenuViewController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2015 RoboVM AB
+ * Copyright (C) 2016 RoboVM AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
